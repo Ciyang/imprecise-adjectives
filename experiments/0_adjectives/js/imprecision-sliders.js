086fc7ce84e3227d4545fc847dc5b9dc1d8d7c03
pratyushaj <pratyushaj@g.ucla.edu>
2018-03-04 12:54:25 -0800

    adding exp files

---
+++ experiments/0_adjectives/js/imprecision-sliders.js
@@ -0,0 +1,313 @@
+
+
+
+
+function make_slides(f) {
+  var   slides = {};
+
+  slides.i0 = slide({
+     name : "i0",
+     start: function() {
+      exp.startT = Date.now();
+     }
+  });
+
+  slides.instructions1 = slide({
+    name : "instructions1",
+    start: function() {
+      $(".instruction_condition").html("Between subject intruction manipulation: "+ exp.instruction);
+    }, 
+    button : function() {
+      exp.go(); //use exp.go() if and only if there is no "present" data.
+    }
+  });
+
+  slides.example = slide({
+    name : "example",
+    present : _.shuffle(stimuli),
+    present_handle : function(stim) {
+      $(".err1").hide();
+      $(".err2").hide();
+      $(".err3").hide();
+      this.init_sliders();      
+      exp.sliderPost = null;
+      // $('input[name="sense"]:checked').attr('checked',false);
+      this.stim = stim; //FRED: allows you to access stim in helpers
+      
+      this.n_sliders = 1;
+//      $(".slider_row").remove();
+//      for (var i=0; i<this.n_sliders; i++) {
+//        var sentence_type_left = this.sentence_types[0];
+//        var sentence_type_left = this.sentence_types[1];        
+//        var sentence_left = sentences[sentence_type_left];
+//        var sentence_right = sentences[sentence_type_right];        
+//        $("#multi_slider_table").append('<tr class="slider_row"><td class="slider_target" id="sentence0">' + "<font size='4'>" + sentence_left + "</font>" + '</td><td colspan="2"><div id="slider0" class="slider">-------[ ]--------</div></td><td class="slider_target" id="sentence1">' + "<font size='4'>" + sentence_right + "</font>" + '</td></tr>');
+//        utils.match_row_height("#multi_slider_table", ".slider_target");
+//      }
+
+    },
+
+    button : function() {
+      var checked_radio  = $('input[name="shapeCheck"]:checked').val();
+
+      if (checked_radio == 'sun' && exp.sliderPost != null){
+        $(".err1").hide();
+        $(".err2").hide();
+        $(".err3").hide();
+        exp.go();
+        //console.log('right set of answers');
+      }
+      else{
+        if (checked_radio != 'sun' && exp.sliderPost != null){
+          $(".err2").hide();
+          $(".err3").hide();
+          $(".err1").show();
+          $('input[name="sense"]:checked').attr('checked',false);
+        }
+        else if (checked_radio == 'sun' && exp.sliderPost == null){
+          $(".err1").hide();
+          $(".err3").hide();
+          $(".err2").show();
+        }
+        else if (checked_radio == undefined || exp.sliderPost == null){
+          $(".err1").hide();
+          $(".err2").hide();
+          $(".err3").show();
+        }
+
+      }
+
+      /*if (checked_radio == 'sun' && exp.sliderPost != null) {
+        //this.log_responses();
+        //_stream.apply(this); //use exp.go() if and only if there is no "present" data.
+        console.log('correct set of answers')
+      } 
+      else 
+      {
+          console.log('incorrect set of answers');
+          $(".err1").show();
+          $('input[name="shapeCheck"]:checked').attr('checked',false);
+      }*/
+        
+    },
+
+    init_sliders : function() {
+      utils.make_slider("#slider0", function(event, ui) {
+        exp.sliderPost = ui.value;
+      });
+    },
+//    make_slider_callback : function(i) {
+//      return function(event, ui) {
+//        exp.sliderPost[i] = ui.value;
+//      };
+//    },
+
+//TO ADD LATER: RESPONSE TIME
+    log_responses : function() {
+        exp.data_trials.push({
+          "slider_response" : exp.sliderPost,
+          "radio_response" : $('input[name="shapeCheck"]:checked').val(),          
+          // "sense" : $('input[name="sense"]:checked').val(),        
+          "slide_number" : exp.phase,
+          "block":"example"
+        });
+    },
+  });
+
+  slides.multi_slider = slide({
+    name : "multi_slider",
+    present : stimuli,
+    present_handle : function(stim) {
+      $(".err").hide();
+      this.init_sliders();
+      //start = Date.now();      
+      exp.sliderPost = null;
+      // $('input[name="sense"]:checked').attr('checked',false);
+      this.stim = stim; //FRED: allows you to access stim in helpers
+
+		  this.n_sliders = 1;
+//      $(".slider_row").remove();
+//      for (var i=0; i<this.n_sliders; i++) {
+//        var sentence_type_left = this.sentence_types[0];
+//        var sentence_type_left = this.sentence_types[1];        
+//        var sentence_left = sentences[sentence_type_left];
+//        var sentence_right = sentences[sentence_type_right];        
+//        $("#multi_slider_table").append('<tr class="slider_row"><td class="slider_target" id="sentence0">' + "<font size='4'>" + sentence_left + "</font>" + '</td><td colspan="2"><div id="slider0" class="slider">-------[ ]--------</div></td><td class="slider_target" id="sentence1">' + "<font size='4'>" + sentence_right + "</font>" + '</td></tr>');
+//        utils.match_row_height("#multi_slider_table", ".slider_target");
+//      }
+      //SLIDER LOAD STIMULI 
+      $(".adj").html(stim.Adj);
+      //var objects = _.shuffle(stim.Objects);
+
+      //QUESTION 1: MULTIPLE CHOICE 
+      var target = stim.Noun;
+      var currDistractors = [];
+      var allDistractors = ["arrow","circle","cloud","cube","cylinder","heart","line","oval","rectangle","spiral","square","star","sun","triangle"];
+
+      currDistractors.push(target);
+      while (currDistractors.length < 4){
+        dist = _.sample(allDistractors);
+        if (currDistractors.indexOf(dist) == -1){
+          currDistractors.push(dist);
+        }
+      }
+
+      multChoiceItems = _.shuffle(currDistractors);
+
+      for (var i = 0; i < multChoiceItems.length; i++){
+        option = multChoiceItems[i];
+        itemID = '.obj' + i;
+
+        if (option.startsWith("a") || option.startsWith("e") || option.startsWith("i") || option.startsWith("o") || option.startsWith("u")){
+          last = 'An ' + option;
+          $(itemID).html(last);
+        }
+        else{
+          last = 'A ' + option;
+          $(itemID).html(last);
+        }
+      }
+
+      //LOADING IMAGES 
+      var src = 'https://ciyang.github.io/imprecise-adjectives/images/';
+
+      for (var j = 0; j < stim.Images.length;j++){
+        img = stim.Images[j];
+        src = 'https://ciyang.github.io/imprecise-adjectives/images/';
+        src+=img;
+        id = 'img' + j;
+
+        if (img.indexOf(stim.Noun)!= -1 && img.indexOf(stim.Color)!= -1){
+          document.getElementById(id).setAttribute('style',"border:2px dashed #CCCCCC;");
+          document.getElementById(id).setAttribute('src',src);
+          console.log('image to be boxed');
+          console.log(img);
+          console.log(id);
+        }
+        else{
+          document.getElementById(id).setAttribute('src',src);
+          console.log('image to be unboxed');
+          console.log(img);
+          console.log(id);
+        }
+      }
+
+
+    },
+
+    button : function() {
+      //end = Date.now();
+      //console.log(end-start);
+      //this.stim = stim;
+      //console.log(stim.Noun);
+      var checked_radio = $('input[name="shapeCheck"]:checked').val();
+    	console.log(exp.sliderPost);
+      // if VALUE == this.stim.CORRECTVALUE {}
+      if (exp.sliderPost != null) {
+        this.log_responses();
+        _stream.apply(this); //use exp.go() if and only if there is no "present" data.
+      } else {
+        $(".err").show();
+        $('input[name="shapeCheck"]:checked').attr('checked',false);
+      }
+    },
+
+    init_sliders : function() {
+      utils.make_slider("#slider1", function(event, ui) {
+        exp.sliderPost = ui.value;
+      });
+    },
+//    make_slider_callback : function(i) {
+//      return function(event, ui) {
+//        exp.sliderPost[i] = ui.value;
+//      };
+//    },
+
+//MUST STILL LOG: RESPONSE TIME, POSITION OF EACH IMAGE IN DISPLAY, DISPLAY ID
+    log_responses : function() {
+        exp.data_trials.push({
+          "slider_response" : exp.sliderPost,
+          "radio_response" : $('input[name="shapeCheck"]:checked').val(),
+          'trial/item_ID' : this.stim.Trial_item_ID,         
+          "condition" : this.stim.Condition,
+          "adj" : this.stim.Adj,
+          "Images" : this.stim.Images,                    
+          // "sense" : $('input[name="sense"]:checked').val(),        
+          "slide_number" : exp.phase,
+          "block":"critical"
+        });
+    },
+  });
+
+  slides.subj_info =  slide({
+    name : "subj_info",
+    submit : function(e){
+      //if (e.preventDefault) e.preventDefault(); // I don't know what this means.
+      exp.subj_data = {
+        language : $("#language").val(),
+        enjoyment : $("#enjoyment").val(),
+        asses : $('input[name="assess"]:checked').val(),
+        age : $("#age").val(),
+        gender : $("#gender").val(),
+        education : $("#education").val(),
+        comments : $("#comments").val(),
+      };
+      exp.go(); //use exp.go() if and only if there is no "present" data.
+    }
+  });
+
+  slides.thanks = slide({
+    name : "thanks",
+    start : function() {
+      exp.data= {
+          "trials" : exp.data_trials,
+          "catch_trials" : exp.catch_trials,
+          "system" : exp.system,
+          //"condition" : exp.condition,
+          "subject_information" : exp.subj_data,
+          "time_in_minutes" : (Date.now() - exp.startT)/60000
+      };
+      setTimeout(function() {turk.submit(exp.data);}, 1000);
+    }
+  });
+
+  return slides;
+}
+
+/// init ///
+function init() {
+  exp.trials = [];
+  exp.catch_trials = [];
+  exp.instruction = _.sample(["instruction1","instruction2"]);
+  exp.system = {
+      Browser : BrowserDetect.browser,
+      OS : BrowserDetect.OS,
+      screenH: screen.height,
+      screenUH: exp.height,
+      screenW: screen.width,
+      screenUW: exp.width
+    };
+  //blocks of the experiment:
+  exp.structure=["i0", "instructions1",'example', 'multi_slider', 'subj_info', 'thanks'];
+  
+  exp.data_trials = [];
+  //make corresponding slides:
+  exp.slides = make_slides(exp);
+
+  exp.nQs = utils.get_exp_length(); //this does not work if there are stacks of stims (but does work for an experiment with this structure)
+                    //relies on structure and slides being defined
+
+  $('.slide').hide(); //hide everything
+
+  //make sure turkers have accepted HIT (or you're not in mturk)
+  $("#start_button").click(function() {
+    if (turk.previewMode) {
+      $("#mustaccept").show();
+    } else {
+      $("#start_button").click(function() {$("#mustaccept").show();});
+      exp.go();
+    }
+  });
+
+  exp.go(); //show first slide
+}
---
+++ experiments/0_adjectives/stimuli/corpus.js
@@ -0,0 +1,91 @@
+// 40 most frequent noun-predicate combinations in the BNC
+
+//[
+//		{"Sentence": "box red", "Predicate": "red", "Noun": "box"},
+//		{"Sentence": "box big", "Predicate": "big", "Noun": "box"}
+//		]
+
+var trials = _.shuffle([
+		{"Trial_item_ID":"1", "Condition":"NoContrast","Noun":"square","Adj":"bumpy","Color":"blue","Images":["bumpy_bluesquare6.jpg", "bumpy_greencircle7.jpg", "tall_yellowcylinder3.jpg", "striped_redtriangle4.jpg"]},
+		{"Trial_item_ID":"2", "Condition":"Contrast","Noun":"circle","Adj":"open","Color":"blue","Images":["open_bluecircle6.jpg", "open_redtriangle7.jpg", "yellow_cloud.jpg", "open_greencircle1.jpg"]},
+		{"Trial_item_ID":"3", "Condition":"NoContrast","Noun":"line","Adj":"bent","Color":"green","Images":["bent_greenline6.jpg", "bent_redarrow7.jpg", "yellow_cloud.jpg", "big_bluesquare1.jpg"]},
+		{"Trial_item_ID":"4", "Condition":"Contrast","Noun":"line","Adj":"curved","Color":"red","Images":["curved_redline6.jpg", "curved_greenarrow7.jpg", "yellow_star.jpg", "curved_blueline1.jpg"]}, 
+		{"Trial_item_ID":"5", "Condition":"NoContrast","Noun":"circle","Adj":"spotted","Color":"yellow","Images":["spotted_yellowcircle6.jpg", "spotted_redsquare7.jpg", "striped_bluetriangle4.jpg", "thick_greenarrow1.jpg"]},
+		{"Trial_item_ID":"6", "Condition":"Contrast","Noun":"square","Adj":"striped","Color":"yellow","Images":["striped_yellowsquare6.jpg", "striped_bluetriangle7.jpg", "spotted_redstar.jpg", "striped_greensquare1.jpg"]},
+		{"Trial_item_ID":"7", "Condition":"NoContrast","Noun":"triangle","Adj":"bumpy","Color":"red","Images":["bumpy_redtriangle6.jpg", "bumpy_bluesquare7.jpg", "spotted_greencircle4.jpg", "full_yellowcylinder2.jpg"]},
+		{"Trial_item_ID":"8", "Condition":"Contrast","Noun":"line","Adj":"curved","Color":"blue","Images":["curved_blueline6.jpg", "curved_redarrow7.jpg", "bumpy_yellowtriangle1.jpg", "curved_greenline1.jpg"]},
+		{"Trial_item_ID":"9", "Condition":"NoContrast","Noun":"square","Adj":"spotted","Color":"yellow","Images":["spotted_yellowsquare6.jpg", "spotted_greentriangle7.jpg", "diagonal_bluearrow.jpg", "striped_redcircle5.jpg"]},
+		{"Trial_item_ID":"10", "Condition":"Contrast","Noun":"triangle","Adj":"striped","Color":"red","Images":["striped_redtriangle6.jpg", "striped_greensquare7.jpg", "chereos_yellowoval.jpg", "striped_bluetriangle1.jpg"]},
+		{"Trial_item_ID":"11", "Condition":"NoContrast","Noun":"circle","Adj":"flat","Color":"green","Images":["bumpy_greencircle2.jpg", "bumpy_yellowtriangle1.jpg", "full_cube8.jpg", "tall_bluespiral7.jpg"]},
+		{"Trial_item_ID":"12", "Condition":"Contrast","Noun":"circle","Adj":"closed","Color":"green","Images":["open_greencircle2.jpg", "open_bluesquare1.jpg", "bumpy_redline.jpg", "open_yellowcircle5.jpg"]},
+		{"Trial_item_ID":"13", "Condition":"NoContrast","Noun":"line","Adj":"straight","Color":"blue","Images":["curved_blueline2.jpg", "curved_greenarrow1.jpg", "spotted_yellowcircle4.jpg", "red_cloud.jpg"]},
+		{"Trial_item_ID":"14", "Condition":"Contrast","Noun":"cube","Adj":"full","Color":"yellow","Images":["full_yellowcube6.jpg", "full_redcylinder7.jpg", "diagonal_bluearrow.jpg", "full_greencube3.jpg"]},
+		{"Trial_item_ID":"15", "Condition":"NoContrast","Noun":"cube","Adj":"empty","Color":"transparent","Images":["full_bluecube1.jpg", "empty_cylinder.jpg", "curved_greenline4.jpg", "blue_bumpyline.jpg"]},
+		{"Trial_item_ID":"16", "Condition":"Contrast","Noun":"triangle","Adj":"flat","Color":"yellow","Images":["bumpy_yellowtriangle2.jpg", "bumpy_bluesquare1.jpg", "green_sphere.jpg", "bumpy_redtriangle5.jpg"]},
+		{"Trial_item_ID":"17", "Condition":"NoContrast","Noun":"circle","Adj":"closed","Color":"blue","Images":["open_bluecircle2.jpg", "open_yellowtriangle1.jpg", "long_redline7.jpg", "green_wavyline.jpg"]},
+		{"Trial_item_ID":"18", "Condition":"Contrast","Noun":"line","Adj":"straight","Color":"red","Images":["bent_redline2.jpg", "bent_greenarrow1.jpg", "chereos_yellowoval.jpg", "bent_blueline5.jpg"]},
+		{"Trial_item_ID":"19", "Condition":"NoContrast","Noun":"cylinder","Adj":"full","Color":"blue","Images":["full_bluecylinder6.jpg", "full_greencube7.jpg", "tall_redspiral4.jpg", "thick_yellowline1.jpg"]},
+		{"Trial_item_ID":"20", "Condition":"Contrast","Noun":"cylinder","Adj":"empty","Color":"transparent","Images":["full_yellowcylinder1.jpg", "empty_cube.jpg", "yellow_cloud.jpg", "full_redcylinder5.jpg"]},
+		{"Trial_item_ID":"21", "Condition":"NoContrast","Noun":"line","Adj":"long","Color":"red","Images":["long_redline5.jpg", "long_greenarrow6.jpg", "yellow_sun.jpg", "open_bluecircle1.jpg"]},
+		{"Trial_item_ID":"22", "Condition":"Contrast","Noun":"line","Adj":"short","Color":"blue","Images":["long_blueline3.jpg", "long_redarrow2.jpg", "yellow_star.jpg", "long_greenline5.jpg"]},
+		{"Trial_item_ID":"23", "Condition":"NoContrast","Noun":"square","Adj":"big","Color":"red","Images":["big_redsquare5.jpg", "big_yellowtriangle6.jpg", "green_wavyline.jpg", "curved_blueline4.jpg"]},
+		{"Trial_item_ID":"24", "Condition":"Contrast","Noun":"triangle","Adj":"small","Color":"yellow","Images":["big_yellowtriangle3.jpg", "big_redsquare2.jpg", "green_wavyline.jpg", "big_bluetriangle5.jpg"]},
+		{"Trial_item_ID":"25", "Condition":"NoContrast","Noun":"oval","Adj":"wide","Color":"green","Images":["wide_greenoval5.jpg", "wide_yellowrectangle6.jpg", "open_bluesquare7.jpg", "bent_redline4.jpg"]},
+		{"Trial_item_ID":"26", "Condition":"Contrast","Noun":"rectangle","Adj":"narrow","Color":"red","Images":["wide_redrectangle3.jpg", "wide_yellowoval2.jpg", "blue_star.jpg", "wide_greenrectangle5.jpg"]},
+		{"Trial_item_ID":"27", "Condition":"NoContrast","Noun":"spiral","Adj":"tall","Color":"blue","Images":["tall_bluespiral5.jpg", "tall_greencylinder6.jpg", "chereos_yellowoval.jpg", "curved_redline4.jpg"]},
+		{"Trial_item_ID":"28", "Condition":"Contrast","Noun":"spiral","Adj":"short","Color":"red","Images":["tall_redspiral3.jpg", "tall_yellowcylinder2.jpg", "full_cube8.jpg", "tall_greenspiral5.jpg"]},
+		{"Trial_item_ID":"29", "Condition":"NoContrast","Noun":"line","Adj":"thick","Color":"yellow","Images":["thick_yellowline5.jpg", "thick_greenarrow6.jpg", "red_sun.jpg", "bumpy_blueshape.jpg"]},
+		{"Trial_item_ID":"30", "Condition":"Contrast","Noun":"line","Adj":"thin","Color":"blue","Images":["thick_blueline3.jpg", "thick_redarrow2.jpg", "bumpy_greencircle4.jpg", "thick_yellowline5.jpg"]},
+		{"Trial_item_ID":"31", "Condition":"NoContrast","Noun":"line","Adj":"long","Color":"green","Images":["long_greenline5.jpg", "long_bluearrow6.jpg", "yellow_cloud.jpg", "spotted_redstar.jpg"]},
+		{"Trial_item_ID":"32", "Condition":"Contrast","Noun":"line","Adj":"short","Color":"red","Images":["long_redline3.jpg", "long_bluearrow2.jpg", "full_yellowcube7.jpg", "long_greenline5.jpg"]},
+		{"Trial_item_ID":"33", "Condition":"NoContrast","Noun":"triangle","Adj":"big","Color":"yellow","Images":["big_yellowtriangle5.jpg", "big_bluesquare6.jpg", "long_redline4.jpg", "green_garabato.jpg"]},
+		{"Trial_item_ID":"34", "Condition":"Contrast","Noun":"square","Adj":"small","Color":"blue","Images":["big_bluesquare3.jpg", "big_yellowtriangle2.jpg", "green_garabato.jpg", "big_redsquare5.jpg"]},
+		{"Trial_item_ID":"35", "Condition":"NoContrast","Noun":"rectangle","Adj":"wide","Color":"yellow","Images":["wide_yellowrectangle5.jpg", "wide_redoval6.jpg", "open_bluetriangle7.jpg", "bent_greenline4.jpg"]},
+		{"Trial_item_ID":"36", "Condition":"Contrast","Noun":"oval","Adj":"narrow","Color":"red","Images":["wide_redoval3.jpg", "wide_greenrectangle2.jpg", "full1_bluesquare2.jpg", "wide_yellowoval5.jpg"]},
+		{"Trial_item_ID":"37", "Condition":"NoContrast","Noun":"cylinder","Adj":"tall","Color":"yellow","Images":["tall_yellowcylinder5.jpg", "tall_bluespiral6.jpg", "bent_redline1.jpg", "green_wavyline.jpg"]},
+		{"Trial_item_ID":"38", "Condition":"Contrast","Noun":"cylinder","Adj":"short","Color":"green","Images":["tall_greencylinder3.jpg", "tall_bluespiral2.jpg", "yellow_cloud.jpg", "tall_redcylinder5.jpg"]},
+		{"Trial_item_ID":"39", "Condition":"NoContrast","Noun":"line","Adj":"thick","Color":"blue","Images":["thick_blueline5.jpg", "thick_redarrow6.jpg", "green_star.jpg", "spotted_yellowsquare4.jpg"]},
+		{"Trial_item_ID":"40", "Condition":"Contrast","Noun":"line","Adj":"thin","Color":"red","Images":["thick_redline3.jpg", "thick_greenarrow2.jpg", "spotted_yellowcircle4.jpg", "thick_blueline5.jpg"]}		
+]);
+
+
+/*var nouns = [
+		{"Noun":"apple", "NounCondition":"food"},
+		{"Noun":"banana", "NounCondition":"food"},
+		{"Noun":"carrot", "NounCondition":"food"},
+		{"Noun":"cheese", "NounCondition":"food"},
+		{"Noun":"tomato", "NounCondition":"food"},								
+		{"Noun":"chair", "NounCondition":"furniture"},								
+		{"Noun":"couch", "NounCondition":"furniture"},								
+		{"Noun":"fan", "NounCondition":"furniture"},								
+		{"Noun":"TV", "NounCondition":"furniture"},								
+		{"Noun":"desk", "NounCondition":"furniture"}								
+];*/
+
+//var stimuli =  makeStims();
+var stimuli = makeStims();
+
+function makeStims() {
+	stims = [];
+
+	for (var i=0; i<trials.length; i++) {
+		images = trials[i].Images;
+
+		ordered_images = _.shuffle(images);
+
+		stims.push(
+			{
+				"Trial_item_ID":trials[i].Trial_item_ID,
+				"Condition":trials[i].Condition,				
+				"Noun":trials[i].Noun,
+				"Adj":trials[i].Adj,
+				"Color":trials[i].Color,
+				"Images":ordered_images,
+				"DisplayID":i
+			}
+			);
+		}
+		
+	return stims;
+	
+}
